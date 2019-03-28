e104e33f6202cd8157c40afd705fee0ddbad4015
zuowenjie <zuowenjie@sunlands.com>
2019-03-28 15:35:39 +0800

    vue-web commit

---
+++ vue-web/src/components/DbSidebar.vue
@@ -0,0 +1,20 @@
+<template>
+  <li class="el-menu-item is-active" style="">{{msg}}</li>
+</template>
+
+<script>
+  export default {
+    name: 'db-sidebar',
+    data() {
+      return {
+        msg: 'Dashboard'
+      }
+    }
+  }
+</script>
+
+<style>
+  li {
+    padding-left: 20px;
+  }
+</style>
