886fb49517d746ddb368064977c061629270c3dd
zuowenjie <zuowenjie@sunlands.com>
2019-03-28 19:04:32 +0800

    web commit

---
+++ web/src/router/index.js
@@ -0,0 +1,33 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+// import HelloWorld from '../components/demo/HelloWorld'
+// import home from '../components/demo/home'
+// import about from '../components/demo/about'
+import Login from '../components/Login'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    // {
+    //   path: '/',
+    //   name: 'HelloWorld',
+    //   component: HelloWorld
+    // },
+    // {
+    //   path: '/home',
+    //   // name: 'home',
+    //   component: home
+    // },
+    // {
+    //   path: '/about',
+    //   // name: 'about',
+    //   component: about
+    // },
+    {
+      path: '/',
+      name: 'Login',
+      component: Login
+    }
+  ]
+})
